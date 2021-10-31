74f1ea03295f533f2bdfc92d27814aaf78f2ed64
Dhruvi Parikh <dp2024@hw.ac.uk>
2021-10-31 13:46:41 +0400

    second commit

--- game.js
+++ game.js
@@ -15,15 +15,21 @@ function Bear() {
         this.htmlElement.style.top = this.y + "px"; 
         this.htmlElement.style.display = "block";
     };
+}
 function start() { 
     //create bear
     bear = new Bear();
     // Add an event listener to the keypress event.
     document.addEventListener("keydown", moveBear, false);
-        }
+    // Add an event listener to the keypress event. document.addEventListener("keydown", moveBear, false); //create new array for bees
+    bees = new Array();
+    //create bees
+    makeBees();
+    }
 
 // Handle keyboad events 
-// to move the bear function moveBear(e) {
+// to move the bear 
+function moveBear(e) {
 //codes of the four keys
 const KEYUP = 38; 
 const KEYDOWN = 40; 
@@ -101,4 +107,66 @@ this.move = function(xDir, yDir) {
             }; 
         }
     }
-}
+    function createBeeImg(wNum) {
+        //get dimension and position of board div
+        let boardDiv = document.getElementById("board"); 
+        let boardDivW = boardDiv.offsetWidth;
+        let boardDivH = boardDiv.offsetHeight;
+        let boardDivX = boardDiv.offsetLeft;
+        let boardDivY = boardDiv.offsetTop;
+        //create the IMG element
+        let img = document.createElement("img");
+        img.setAttribute("src", "images/bee.gif"); 
+        img.setAttribute("width", "100");
+        img.setAttribute("alt", "A bee!");
+        img.setAttribute("id", "bee" + wNum);
+        img.setAttribute("class", "bee"); //set class of html tag img 
+        //add the IMG element to the DOM as a child of the board div 
+        img.style.position = "absolute";
+        boardDiv.appendChild(img);
+        //set initial position
+        let x = getRandomInt(boardDivW);
+        let y = getRandomInt(boardDivH); 
+        img.style.left = (boardDivX + x) + "px"; 
+        img.style.top = (y) + "px";
+        //return the img object
+        return img;
+        }
+        function makeBees() {
+            //get number of bees specified by the user
+            let nbBees = document.getElementById("nbBees").value;
+            nbBees = Number(nbBees); //try converting the content of the input to a number if (isNaN(nbBees)) { //check that the input field contains a valid number
+            window.alert("Invalid number of bees"); return;
+            }
+            //create bees
+            let i = 1;
+            while (i <= nbBees) {
+            var num = i;
+            var bee = new Bee(num); //create object and its IMG element bee.display(); //display the bee
+            bees.push(bee); //add the bee object to the bees array
+            i++;
+            } 
+         
+ 
+            function moveBees() {
+            //get speed input field value
+            let speed = document.getElementById("speedBees").value; 
+            //move each bee to a random location
+            for (let i = 0; i < bees.length; i++) {
+            let dx = getRandomInt(2 * speed) - speed; 
+            let dy = getRandomInt(2 * speed) - speed; 
+            bees[i].move(dx, dy);
+            } 
+        }
+        function updateBees() { // update loop for game //move the bees randomly
+            moveBees();
+            //use a fixed update period
+            let period = 10;//modify this to control refresh period //update the timer for the next move
+            updateTimer = setTimeout('updateBees()', period);
+            }
+    } 
+
+            
+            
+        
+    
